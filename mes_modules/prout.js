db331df7f58c51fcf9d4128a220860390371244d
Fwancis <al.la.science@gmail.com>
2018-03-23 21:20:56 +0100

    bug fix

--- mes_modules/prout.js
+++ mes_modules/prout.js
@@ -6,19 +6,19 @@ var doProut = function(message){
   if (notifs.length == 1){
     message.channel.send("Tu pues " + notifs[0] + " ! \nSigné : " + message.author);
   }else if (notifs.length >1){
-    var reponse = getMultiProut(notifs);
+    var reponse = getMultiProut(notifs, message.author);
     message.channel.send(reponse);
   }else{
     message.reply(", tu pues ! ");
   }
 }
 
-var getMultiProut = function(notifies){
+var getMultiProut = function(notifies, auteur){
   var message = "Vous puez " + notifies[0]
   for (var i = 1; i < notifies.length-1; i++) {
     message += ", " + notifies[i];
   }
-  message += " et " + notifies[notifies.length-1] + " ! \nSigné : " + message.author;
+  message += " et " + notifies[notifies.length-1] + " ! \nSigné : " + auteur;
   return message;
 }
 
